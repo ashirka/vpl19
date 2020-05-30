2536817e11db4043cf9d78d690e28d0235a98d9b
alenashuvar <alenashuvar@users.noreply.github.com>
2020-05-31 00:09:59 +0300

    update code

--- Егорова_Шувар/local_search_system.py
+++ Егорова_Шувар/local_search_system.py
@@ -6,10 +6,15 @@
 быть ранжированы: чем чаще в данном тексте встретилось данное слово,
 тем выше текст в выдаче'''
 '''Схема работы:
-1. эслауэр - перевод ответа в нижние буковки
+1. экслауэр - перевод ответа в нижние буковки
 2. открываем текстовый файл, ищем слово/слова, считаем кол-во, кидаем в список (или словарь как пойдет)
 3. выводим название файла - название статьи - отрывок с искомым словом (сколько слов найдено - столько и 
-отрывков) -  название другого файла и тд. по частоте встречания'''
+отрывков) -  название другого файла и тд. по частоте встречания
+
+мы разделяем по регулярке слова, потом у нас есть список со строками, и мы из них вычленяем по 10 слов
+с первой строки - с конца, с последней строки - с начала, в остальных - с обеихь сторон при эжтом если там меньше 20
+слов, то мы выдергиваем всю эту строку'''
+
 import re
 
 paradigm_adj = 'ый ий ой ая ое ые ие ого его ой ей ых их ому ему им ым ую юю ими ыми ом ем'
@@ -44,14 +49,15 @@ for i in range(1, 12):
         regex_title = r'^.+$'
         text_title = re.search(regex_title, text, re.M)
         text_title = text_title.group()
-        text = text.lower()
+        #text = text.lower()
         if len(user_words) == 1:
             rex_user = ' ' + word1[:-1] + '[а-я]{0,3}' + ' '
-            regex_user = re.compile(rex_user)
-            found_match = re.findall(regex_user, text)
-            print(found_match)
+            #regex_user = re.compile(rex_user)
+            found_match = re.findall(rex_user, text, re.I)
+            #print(found_match)
             found_numbers = len(found_match)
-            titles_number[text_title] = found_numbers
+            titles_number[text_title] = [found_numbers]
+            titles_number[text_title] += [file]
         elif len(user_words) == 2:
             if (word1[-3:] in paradigm_adj) or (word2[-3:] in paradigm_adj):
                 if word1[-3:] in paradigm_adj:
@@ -62,12 +68,18 @@ for i in range(1, 12):
                 rex_user = word1[:-2] + '[а-я]{0,3}' + '\s(\S)*( )*' + word2[:-2] + '[а-я]{0,3}'
             else:
                 rex_user = word1[:-1] + '[а-я]{0,3}' + '\s(\S)*( )*' + word2[:-1] + '[а-я]{0,3}'
-            regex_user = re.compile(rex_user)
-            found_match = re.findall(regex_user, text)
+            #regex_user = re.compile(rex_user)
+            found_match = re.findall(rex_user, text, re.I)
+            #print(found_match)
             found_numbers = len(found_match)
             titles_number[text_title] = found_numbers
+        #list = text.split()
+        #print(list)
+        '''gh = r'(\W*[А-Яа-яA-Za-zё\d]+\W*){3}холод'
+        jaerbf = re.findall(gh, text)
+        print(jaerbf)'''
 
-gh = '(\W*[А-Яа-яA-Za-zё\d]+\W*){10}'
+
 list_titles_number = list(titles_number.items())
 list_titles_number.sort(key=lambda i: i[1])
 list_titles_number = list_titles_number[::-1]
